a208f2177fa361064376338ef42b0d955ef649dc
Dmytro Zamosenchuk <Dmytro Zamosenchuk>
2016-10-30 19:46:57 +0200

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dzamo on 30/10/2016.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Dmytro");
+        System.out.println("Zamosenchuk");
+    }
+}
