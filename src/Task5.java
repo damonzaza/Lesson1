044fc1a0bb1b828efa2928496089b0e337a66d83
damonzaza <d.zamosenchuk@gmail.com>
2016-10-31 21:43:00 +0200

    Save task 5

---
+++ src/Task5.java
@@ -0,0 +1,28 @@
+/**
+ * Написати програму, яка запитатиме користувача ввести два цілих числа і виконає над ними операції з 4 завдання.
+ * Created by Dmytro Zamosenchuk on 31/10/2016.
+ */
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int a=0;
+        int b=0;
+        double Result=0;
+        System.out.println("This is calculator");
+        //a=(int)(Math.random()*100);
+        //b=(int)(Math.random()*100);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input first integer ");
+        a = in.nextInt();
+        System.out.print("Input second integer ");
+        b = in.nextInt();
+        System.out.println("Sum is "+ (a+b));
+        System.out.println("Sum is "+ (a-b));
+        System.out.println("Sum is "+ (a*b));
+        System.out.println("Sum is "+ (a/b));
+        System.out.println("Sum is "+ (a%b));
+    }
+}
