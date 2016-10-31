691c2eb920ae628948a8f53490185b3627f40e47
damonzaza <d.zamosenchuk@gmail.com>
2016-10-31 21:24:05 +0200

    Save task 4

---
+++ src/Task4.java
@@ -0,0 +1,13 @@
+/**
+ * Допишіть програму, яка знайде об'єм прямокутного паралелепіпеда:
+ * https://gist.github.com/FreeFly19/bd47696be9ce10e220837640a3a31f8f
+ * Created by Dmytro Zamosenchuk on 31/10/2016.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        float a = 5, b = 3f, h = 3.5f;// a - довжина, b - ширина, h - висота
+        float V = a*b*h;
+
+        System.out.println(V);
+    }
+}
